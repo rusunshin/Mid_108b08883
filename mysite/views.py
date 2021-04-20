361b4973b03a7020a7c2001d79169d4f9e65c990
CJCU-CC <108b08883@mailst.cjcu.ecu.tw>
2021-04-20 09:33:51 +0800

    commit

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from mysite import models
 
 
 # Create your views here.
 def index(request):
+    posts = models.Post.objects.filter(enabled=True).order_by('-pub_time')[:30]
+    moods = models.Mood.objects.all()
     return render(request, 'index.html', locals())
